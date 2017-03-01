9e5e9959a00967d2376cc8ddecf27205e1b83a4e
PatrickJS <github@gdi2290.com>
2017-02-28 19:05:50 -0500

    refactor(home): OnDestroy (#3)

--- src/app/home/home-view.component.ts
+++ src/app/home/home-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CachedHttp } from '../../modules/cached-http/cached-http';
 import { Observable } from 'rxjs/Observable';
 
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs/Observable';
 	selector: 'home-view',
 	template: `<h3>{{subs | async}}</h3>`
 })
-export class HomeView implements OnInit {
+export class HomeView implements OnInit, OnDestroy {
   public subs: Observable<string>;
 
   constructor(private http: CachedHttp) {}
@@ -15,5 +15,10 @@ export class HomeView implements OnInit {
     this.subs = this.http.get('http://localhost:8000/data').map(data => {
       return `${data.greeting} ${data.name}`;
     });
+  }
+  ngOnDestroy() {
+    if (this.subs && this.subs.unsubscribe) {
+      this.subs.unsubscribe();
+    }
   }
 }
