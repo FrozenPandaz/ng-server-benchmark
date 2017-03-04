869e2dc37b5ccf2934d4ed12e03752bc15d043be
FrozenPandaz <jasonjean1993@gmail.com>
2017-03-04 17:54:18 -0500

    Cleanup transfer state impl

--- src/app/server-app.module.ts
+++ src/app/server-app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule, Renderer, RendererFactoryV2, ViewEncapsulation } from '@angular/core';
-import { ServerModule, PlatformState } from '@angular/platform-server';
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
 import { ServerTransferStateModule } from '../modules/transfer-state/server-transfer-state.module';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
@@ -15,34 +15,9 @@ import { TransferState } from '../modules/transfer-state/transfer-state';
 })
 export class ServerAppModule {
 
-  constructor(private transferState: TransferState, private state: PlatformState, private rendererFactory: RendererFactoryV2) { }
+  constructor(private transferState: TransferState) { }
 
   ngOnBootstrap() {
-    this.injectTransferState();
-  }
-
-  /**
-   * Inject the Universal Cache into the bottom of the <head>
-   */
-  private injectTransferState() {
-    try {
-      const document: any = this.state.getDocument();
-      const transferStateString = JSON.stringify(this.transferState.toJson());
-      const renderer = this.rendererFactory.createRenderer(document, {
-        id: '-1',
-        encapsulation: ViewEncapsulation.None,
-        styles: [],
-        data: {}
-      });
-      const script = renderer.createElement('script');
-      renderer.setValue(script, `window['TRANSFER_STATE'] = ${transferStateString}`);
-      const head = document.children[0].children[0];
-      if (head.name !== 'head') {
-        throw new Error('Please have <head> as the first element in your document');
-      }
-      renderer.appendChild(head, script);
-    } catch (e) {
-      console.error(e);
-    }
+    this.transferState.inject();
   }
 }
